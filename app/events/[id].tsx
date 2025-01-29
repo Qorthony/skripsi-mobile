01e180f0e35265e6e109d88491233ca4c20ea984
ahmad.qorthony <ahmad.qorthony@gmail.com>
2025-01-29 18:03:51 +0700

    counter button interaction done

--- app/events/[id].tsx
+++ app/events/[id].tsx
@@ -4,6 +4,14 @@ import { useLocalSearchParams } from 'expo-router'
 import { EVENTS_DATA, TicketTypes } from '@/constants/events-data';
 import dayjs from 'dayjs';
 import CounterButton from '@/components/CounterButton';
+import AbsoluteBottomView from '@/components/AbsoluteBottomView';
+
+type selectedTicket = {
+    id: number;
+    name: string;
+    price: number;
+    quantity: number;
+}
 
 export default function DetailEvent() {
     const { id } = useLocalSearchParams();
@@ -11,67 +19,125 @@ export default function DetailEvent() {
 
     const [activeTab, setActiveTab] = useState('deskripsi');
     const activeTabClassName = 'border-b-2 border-purple-600';
-    
+
+    const [selected, setSelected] = useState<selectedTicket[]>([]);
+
     useEffect(() => {
         console.log(id);
     }, [id]);
 
-  return (
-    <SafeAreaView className='flex-1 bg-white'>
-        <View className='flex-1 bg-white'>
-            <Image 
-                source={EVENTS_DETAIL?.image} 
-                className='w-full h-48 rounded-lg' 
-            />
-            <View className='px-4 py-2'>
-                <Text className='text-xl font-bold'>{EVENTS_DETAIL?.name}</Text>
-                <Text className='text-sm text-gray-600'>{dayjs(EVENTS_DETAIL?.date).format('DD MMMM YYYY') }</Text>
-                {
-                    EVENTS_DETAIL?.location==='online' ? 
-                    <Text className='text-sm text-gray-600'>{EVENTS_DETAIL?.location}</Text>:
-                    <Text className='text-sm text-gray-600'>{EVENTS_DETAIL?.city}</Text>
-                }
-            </View>
 
-            <View className='bg-gray-100 d-flex justify-around flex-row'>
-                <Pressable 
-                    className={`flex-grow py-3 ${activeTab === 'deskripsi' ? activeTabClassName : ''}`}
-                    onPress={() => setActiveTab('deskripsi')}
-                >
-                    <Text className='text-center'>Deskripsi</Text>
-                </Pressable>
-                <Pressable 
-                    className={`flex-grow py-3 ${activeTab === 'tiket' ? activeTabClassName : ''}`}
-                    onPress={() => setActiveTab('tiket')}
-                >
-                    <Text className='text-center'>Tiket</Text>
-                </Pressable>
-            </View>
-            <View className='flex-1 p-4'>
-                {
-                    activeTab === 'deskripsi' ? 
-                    <Text>{EVENTS_DETAIL?.description}</Text>:
-                    <FlatList
-                        className='mb-14'
-                        showsVerticalScrollIndicator={false}
-                        data={EVENTS_DETAIL?.tickets}
-                        renderItem={({ item }) => (
-                            <TicketCard {...item} />
-                        )}
-                        keyExtractor={item => item.id.toString()}
-                    />
-                }
-            </View>
 
-            <View className='absolute bottom-0 left-0 right-0 bg-purple-600 p-4'>
-                <Text className='text-center text-lg'>This is a sticky footer</Text>
+    return (
+        <SafeAreaView className='flex-1 bg-white'>
+            <View className='flex-1 bg-white'>
+                <Image
+                    source={EVENTS_DETAIL?.image}
+                    className='w-full h-48 rounded-lg'
+                />
+                <View className='px-4 py-2'>
+                    <Text className='text-xl font-bold'>{EVENTS_DETAIL?.name}</Text>
+                    <Text className='text-sm text-gray-600'>{dayjs(EVENTS_DETAIL?.date).format('DD MMMM YYYY')}</Text>
+                    {
+                        EVENTS_DETAIL?.location === 'online' ?
+                            <Text className='text-sm text-gray-600'>{EVENTS_DETAIL?.location}</Text> :
+                            <Text className='text-sm text-gray-600'>{EVENTS_DETAIL?.city}</Text>
+                    }
+                </View>
+
+                <View className='bg-gray-100 d-flex justify-around flex-row'>
+                    <Pressable
+                        className={`flex-grow py-3 ${activeTab === 'deskripsi' ? activeTabClassName : ''}`}
+                        onPress={() => setActiveTab('deskripsi')}
+                    >
+                        <Text className='text-center'>Deskripsi</Text>
+                    </Pressable>
+                    <Pressable
+                        className={`flex-grow py-3 ${activeTab === 'tiket' ? activeTabClassName : ''}`}
+                        onPress={() => setActiveTab('tiket')}
+                    >
+                        <Text className='text-center'>Tiket</Text>
+                    </Pressable>
+                </View>
+                <View className='flex-1 p-4'>
+                    {
+                        activeTab === 'deskripsi' ?
+                            <Text>{EVENTS_DETAIL?.description}</Text> :
+                            <FlatList
+                                className='mb-16'
+                                showsVerticalScrollIndicator={false}
+                                data={EVENTS_DETAIL?.tickets}
+                                renderItem={({ item }) => (
+                                    <TicketCard
+                                        selected={selected}
+                                        setSelected={setSelected} 
+                                        {...item} 
+                                    />
+                                )}
+                                keyExtractor={item => item.id.toString()}
+                            />
+                    }
+                </View>
+
+                <AbsoluteBottomView>
+                    <View className='flex-row justify-between'>
+                        <Text>Total</Text>
+                        <Text>Rp. {selected.reduce((acc, curr) => acc + (curr.price * curr.quantity), 0)}</Text>
+                    </View>
+                    <Pressable className='bg-purple-600 rounded-lg p-2'>
+                        <Text className='text-white text-center'>Beli Tiket</Text>
+                    </Pressable>
+                </AbsoluteBottomView>
             </View>
-        </View>
-    </SafeAreaView>
-  )
+        </SafeAreaView>
+    )
 }
 
-function TicketCard({ type, price, description }: TicketTypes) {
+interface TicketCardProps {
+    id: number;
+    type: string;
+    price: number;
+    description: string;
+    selected: selectedTicket[];
+    setSelected: (value: any) => void;
+}
+
+function TicketCard({
+    id, 
+    type, 
+    price, 
+    description,
+    selected,
+    setSelected
+}: TicketCardProps) {
+
+    const handleSubstract = () => {
+        // jika quantity 1, maka hapus item dari selected. jika tidak, kurangi quantity
+        if (selected.find((item: any) => item.id === id)?.quantity === 1) {
+            setSelected(selected.filter((item: any) => item.id !== id));
+            return;
+        }
+        setSelected(selected.map((item: any) => {
+            if (item.id === id) {
+                return { ...item, quantity: item.quantity - 1 }
+            }
+            return item;
+        }));
+    }
+
+    const handleAdd = () => {
+        if (selected.find((item: any) => item.id === id)) {
+            setSelected(selected.map((item: any) => {
+                if (item.id === id) {
+                    return { ...item, quantity: item.quantity + 1 }
+                }
+                return item;
+            }));
+            return;
+        }
+        setSelected([...selected, { id, name: type, price, quantity: 1 }]);
+    }
+    
     return (
         <View className='bg-slate-200 rounded-lg p-2 my-2'>
             <View>
@@ -79,7 +145,11 @@ function TicketCard({ type, price, description }: TicketTypes) {
                 <Text className='text-sm'>Rp. {price}</Text>
                 <Text className='text-sm text-gray-600'>{description}</Text>
                 <View className='items-end'>
-                    <CounterButton />
+                    <CounterButton
+                        onSubstract={handleSubstract}
+                        onAdd={handleAdd}
+                        value={selected.find((item: any) => item.id === id)?.quantity || 0}
+                    />
                 </View>
             </View>
         </View>
--- components/CounterButton.tsx
+++ components/CounterButton.tsx
@@ -2,17 +2,27 @@ import { View, Text, Pressable } from 'react-native'
 import React from 'react'
 import AntDesign from '@expo/vector-icons/AntDesign';
 
-export default function CounterButton() {
+interface CounterButtonProps {
+    onSubstract: () => void;
+    onAdd: () => void;
+    value: number;
+}
+
+export default function CounterButton({
+    onSubstract,
+    onAdd,
+    value
+}: CounterButtonProps) {
     return (
         <View className='flex-row items-center'>
             <Pressable
-                onPress={() => console.log('minus')}
+                onPress={onSubstract}
             >
                 <AntDesign name="minuscircleo" size={14} color="black" />
             </Pressable>
-            <Text className='px-2 text-sm'>0</Text>
+            <Text className='px-2 text-sm'>{value }</Text>
             <Pressable
-                onPress={() => console.log('plus')}
+                onPress={onAdd}
             >
                 <AntDesign name="pluscircleo" size={14} color="black" />
             </Pressable>
